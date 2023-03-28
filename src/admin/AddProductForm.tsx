cf7f071228525985a3c0dbe8fda26f8cecdc6aff
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-03-28 14:04:41 +0300

    add product form connected to backend

--- src/admin/AddProductForm.tsx
+++ src/admin/AddProductForm.tsx
@@ -1,14 +1,49 @@
-// src/admin/AddProductForm.tsx
-
 import React, { useState } from 'react';
+
+
+
+const API_URL = 'http://localhost:3001/products/add'; // Replace with your API URL
+
+async function addProduct(product: any) {
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(product),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to add product: ${response.statusText}`);
+    }
+
+    return await response.json();
+}
 
 const AddProductForm: React.FC = () => {
     const [name, setName] = useState('');
-    // Add more states for other fields like description, price, category, and image_url
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('');
+    const [image_url, setImageUrl] = useState('');
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        // Handle form submission, make a request to the API to add a product
+
+        try {
+            const product = {
+                name,
+                description,
+                price,
+                category,
+                image_url,
+            };
+
+            const result = await addProduct(product);
+            console.log('Product added successfully:', result);
+        } catch (error) {
+            console.error('Error adding product:', error);
+        }
     };
 
     return (
@@ -25,6 +60,46 @@ const AddProductForm: React.FC = () => {
                 />
             </div>
             {/* Add more input fields for other fields like description, price, category, and image_url */}
+            <div>
+                <label htmlFor="description" className="block text-sm font-medium">Description</label>
+                <textarea
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="mt-1 w-full border border-gray-300 rounded p-2"
+                />
+            </div>
+            <div>
+                <label htmlFor="price" className="block text-sm font-medium">Price</label>
+                <input
+                    type="number"
+                    step="0.01"
+                    id="price"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    className="mt-1 w-full border border-gray-300 rounded p-2"
+                />
+            </div>
+            <div>
+                <label htmlFor="category" className="block text-sm font-medium">Category</label>
+                <input
+                    type="text"
+                    id="category"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    className="mt-1 w-full border border-gray-300 rounded p-2"
+                />
+            </div>
+            <div>
+                <label htmlFor="image_url" className="block text-sm font-medium">Image URL</label>
+                <input
+                    type="text"
+                    id="image_url"
+                    value={image_url}
+                    onChange={(e) => setImageUrl(e.target.value)}
+                    className="mt-1 w-full border border-gray-300 rounded p-2"
+                />
+            </div>
             <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded">
                 Add Product
             </button>
