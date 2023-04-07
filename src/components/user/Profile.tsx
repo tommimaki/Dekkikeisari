de88379108187d1b854d3385af931c089725d82a
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-04-07 19:51:14 +0300

    profile page styling

--- src/components/user/Profile.tsx
+++ src/components/user/Profile.tsx
@@ -1,13 +1,149 @@
+// import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+// import { useSelector } from 'react-redux';
+// import { RootState } from '../../store';
+
+// const Profile = () => {
+//     const [user, setUser] = useState({ name: '', email: '', address: '', id: null, });
+//     const [isEditing, setIsEditing] = useState(false);
+//     // Fetch user data from the server when the component mounts
+//     useEffect(() => {
+//         fetchUserData().then((userData) => setUser(userData));
+//     }, []);
+
+//     const fetchUserData = async () => {
+//         try {
+//             const response = await fetch("http://localhost:3001/users/user", {
+//                 headers: {
+//                     Authorization: `Bearer ${localStorage.getItem("token")}`,
+//                 },
+//             });
+
+//             if (!response.ok) {
+//                 throw new Error("Failed to fetch user data");
+//             }
+
+//             const userData = await response.json();
+//             console.log("Fetched user data:", userData);
+//             if (userData.name === undefined) {
+//                 userData.name = '';
+//             }
+//             return userData;
+//         } catch (error) {
+//             console.error("Error fetching user data:", error);
+//             throw error;
+//         }
+//     };
+
+
+//     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//         const { name, value } = e.target;
+//         setUser({ ...user, [name]: value });
+//     };
+//     const handleSubmit = async (e: FormEvent) => {
+//         e.preventDefault();
+//         try {
+//             const response = await fetch("http://localhost:3001/users/user", {
+//                 method: "PUT",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     Authorization: `Bearer ${localStorage.getItem("token")}`,
+//                 },
+//                 body: JSON.stringify(user),
+//             });
+
+//             if (!response.ok) {
+//                 throw new Error("Failed to update user data");
+//             }
+
+//             const updatedUser = await response.json(); // updated user data from the response
+//             setUser(updatedUser); // Update the local state with the updated user data
+
+//             console.log("Updated user data:", user);
+//             setIsEditing(false);
+//         } catch (error) {
+//             console.error(`Error updating user data: ${error}`);
+//         }
+//     };
+
+
+
+//     return (
+//         <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+//             <div className="bg-white p-8 rounded shadow-lg w-full max-w-md">
+//                 <h2 className="text-2xl font-bold mb-4">Hi, {user.name}</h2>
+//                 {isEditing ? (
+//                     <form onSubmit={handleSubmit}>
+//                         <div className="mb-4">
+//                             <label htmlFor="name" className="block text-sm mb-1">
+//                                 Name:
+//                             </label>
+//                             <input
+//                                 id="name"
+//                                 name="name"
+//                                 type="text"
+//                                 value={user.name}
+//                                 onChange={handleChange}
+//                                 className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+//                             />
+//                         </div>
+//                         <div className="mb-4">
+//                             <label htmlFor="email" className="block text-sm mb-1">
+//                                 Email:
+//                             </label>
+//                             <input
+//                                 id="email"
+//                                 name="email"
+//                                 type="email"
+//                                 value={user.email}
+//                                 onChange={handleChange}
+//                                 className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+//                             />
+//                         </div>
+//                         <button
+//                             type="submit"
+//                             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none"
+//                         >
+//                             Save Changes
+//                         </button>
+//                         <button
+//                             type="button"
+//                             onClick={() => setIsEditing(false)}
+//                             className="bg-gray-300 text-black px-4 py-2 ml-2 rounded hover:bg-gray-400 focus:outline-none"
+//                         >
+//                             Cancel
+//                         </button>
+//                     </form>
+//                 ) : (
+//                     <div>
+//                         <p className="mb-2">
+//                             <span className="font-semibold">Name:</span> {user.name}
+//                         </p>
+//                         <p className="mb-4">
+//                             <span className="font-semibold">Email:</span> {user.email}
+//                         </p>
+//                         <button
+//                             onClick={() => setIsEditing(true)}
+//                             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none"
+//                         >
+//                             Edit Profile
+//                         </button>
+//                     </div>
+//                 )}
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Profile;
+
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import { FaUser, FaEnvelope, FaMapMarkerAlt, FaCreditCard } from 'react-icons/fa';
 
 const Profile = () => {
-    const [user, setUser] = useState({ name: '', email: '', id: null });
-
+    const [user, setUser] = useState({ name: '', email: '', address: '', id: null });
     const [isEditing, setIsEditing] = useState(false);
-    // const token = useSelector((state: RootState) => state.auth.token);
-
     // Fetch user data from the server when the component mounts
     useEffect(() => {
         fetchUserData().then((userData) => setUser(userData));
@@ -27,9 +163,15 @@ const Profile = () => {
 
             const userData = await response.json();
             console.log("Fetched user data:", userData);
-            if (userData.name === undefined) {
-                userData.name = '';
-            }
+
+            //sets empty string if datafield null
+            const propertiesToCheck = ["name", "email", "address", "id"];
+            propertiesToCheck.forEach((property) => {
+                if (userData[property] === undefined) {
+                    console.log(property)
+                    userData[property] = '';
+                }
+            });
             return userData;
         } catch (error) {
             console.error("Error fetching user data:", error);
@@ -42,6 +184,7 @@ const Profile = () => {
         const { name, value } = e.target;
         setUser({ ...user, [name]: value });
     };
+
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
         try {
@@ -69,15 +212,16 @@ const Profile = () => {
     };
 
 
-
     return (
         <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-            <div className="bg-white p-8 rounded shadow-lg w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-4">Hi, {user.name}</h2>
+            <div className="bg-white min-h-screen p-8 rounded shadow-lg w-full text-left max-w-md">
+                <h2 className="text-2xl text-center font-bold mb-4">Hi, {user.name}</h2>
+                <hr className="mb-20" />
                 {isEditing ? (
                     <form onSubmit={handleSubmit}>
                         <div className="mb-4">
                             <label htmlFor="name" className="block text-sm mb-1">
+                                <FaUser className="inline-block mr-2" />
                                 Name:
                             </label>
                             <input
@@ -91,6 +235,7 @@ const Profile = () => {
                         </div>
                         <div className="mb-4">
                             <label htmlFor="email" className="block text-sm mb-1">
+                                <FaEnvelope className="inline-block mr-2" />
                                 Email:
                             </label>
                             <input
@@ -99,6 +244,34 @@ const Profile = () => {
                                 type="email"
                                 value={user.email}
                                 onChange={handleChange}
+                                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="address" className="block text-sm mb-1">
+                                <FaMapMarkerAlt className="inline-block mr-2" />
+                                Address:
+                            </label>
+                            <input
+                                id="address"
+                                name="address"
+                                type="text"
+                                value={user.address}
+                                onChange={handleChange}
+                                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="card" className="block text-sm mb-1">
+                                <FaCreditCard className="inline-block mr-2" />
+                                Payment Information:
+                            </label>
+                            <input
+                                id="card"
+                                name="card"
+                                type="text"
+                                value="**** **** **** 1234"
+                                disabled
                                 className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
                             />
                         </div>
@@ -119,11 +292,21 @@ const Profile = () => {
                 ) : (
                     <div>
                         <p className="mb-2">
-                            <span className="font-semibold">Name:</span> {user.name}
+                            <span className="font-semibold"><FaUser className="inline-block mr-2" /> Name:</span> {user.name}
                         </p>
+                        <hr className="mb-4" />
+                        <p className="mb-2">
+                            <span className="font-semibold"><FaEnvelope className="inline-block mr-2" /> Email:</span> {user.email}
+                        </p>
+                        <hr className="mb-4" />
+                        <p className="mb-2">
+                            <span className="font-semibold"><FaMapMarkerAlt className="inline-block mr-2" /> Address:</span> {user.address}
+                        </p>
+                        <hr className="mb-4" />
                         <p className="mb-4">
-                            <span className="font-semibold">Email:</span> {user.email}
+                            <span className="font-semibold"><FaCreditCard className="inline-block mr-2" /> Payment Information:</span> **** **** **** 1234
                         </p>
+                        <hr className="mb-4" />
                         <button
                             onClick={() => setIsEditing(true)}
                             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none"
