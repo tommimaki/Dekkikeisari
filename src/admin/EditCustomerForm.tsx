364195b7e18d8745b95a6d57543125ec8df128fa
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-04-11 11:19:08 +0300

    edit customer form rework

--- src/admin/EditCustomerForm.tsx
+++ src/admin/EditCustomerForm.tsx
@@ -1,3 +1,67 @@
+// import React, { useState } from 'react';
+// import Customer from '../interfaces/customer';
+
+// interface EditCustomerProps {
+//     customer: Customer;
+//     onCloseModal: () => void;
+//     onCustomerUpdated: () => void;
+// }
+
+// const EditCustomer: React.FC<EditCustomerProps> = ({ customer, onCloseModal, onCustomerUpdated }) => {
+//     const [updatedCustomer, setUpdatedCustomer] = useState<Customer>(customer);
+
+//     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//         setUpdatedCustomer({ ...updatedCustomer, [event.target.name]: event.target.value });
+//     };
+
+//     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//         try {
+//             await fetch(`http://localhost:3001/user/${updatedCustomer.id}`, {
+//                 method: 'PUT',
+//                 headers: { 'Content-Type': 'application/json' },
+//                 body: JSON.stringify(updatedCustomer),
+//             });
+//             onCustomerUpdated();
+//             onCloseModal();
+//         } catch (error) {
+//             console.error('Error updating customer:', error);
+//         }
+//     };
+
+//     return (
+//         <div>
+//             <h2>Edit Customer</h2>
+//             <form className="flex flex-col w-full max-w-md mx-auto space-y-4">
+//                 <input
+//                     type="text"
+//                     className="form-input px-3 py-2 border border-gray-300 rounded"
+//                     placeholder="Name"
+//                 />
+//                 <input
+//                     type="email"
+//                     className="form-input px-3 py-2 border border-gray-300 rounded"
+//                     placeholder="Email"
+//                 />
+//                 <input
+//                     type="text"
+//                     className="form-input px-3 py-2 border border-gray-300 rounded"
+//                     placeholder="Address"
+//                 />
+//                 <select className="form-select px-3 py-2 border border-gray-300 rounded">
+//                     <option value="customer">Customer</option>
+//                     <option value="admin">Admin</option>
+//                 </select>
+//                 <button className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600">
+//                     Submit
+//                 </button>
+//             </form>
+
+//         </div>
+//     );
+// };
+
+// export default EditCustomer;
 import React, { useState } from 'react';
 import Customer from '../interfaces/customer';
 
@@ -10,7 +74,7 @@ interface EditCustomerProps {
 const EditCustomer: React.FC<EditCustomerProps> = ({ customer, onCloseModal, onCustomerUpdated }) => {
     const [updatedCustomer, setUpdatedCustomer] = useState<Customer>(customer);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         setUpdatedCustomer({ ...updatedCustomer, [event.target.name]: event.target.value });
     };
 
@@ -31,37 +95,51 @@ const EditCustomer: React.FC<EditCustomerProps> = ({ customer, onCloseModal, onC
 
     return (
         <div>
-            <h2>Edit Customer</h2>
-            <form className="flex flex-col w-full max-w-md mx-auto space-y-4">
+            <div className='flex  justify-between'>
+
+                <h2>Edit Customer</h2>
+                <button onClick={onCloseModal} className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600">
+                    X
+                </button>
+            </div>
+            <form onSubmit={handleSubmit} className="flex flex-col w-full max-w-md mx-auto space-y-4">
                 <input
                     type="text"
+                    name="name"
+                    value={updatedCustomer.name}
+                    onChange={handleChange}
                     className="form-input px-3 py-2 border border-gray-300 rounded"
                     placeholder="Name"
                 />
                 <input
                     type="email"
+                    name="email"
+                    value={updatedCustomer.email}
+                    onChange={handleChange}
                     className="form-input px-3 py-2 border border-gray-300 rounded"
                     placeholder="Email"
                 />
                 <input
-                    type="password"
-                    className="form-input px-3 py-2 border border-gray-300 rounded"
-                    placeholder="Password"
-                />
-                <input
                     type="text"
+                    name="address"
+                    value={updatedCustomer.address}
+                    onChange={handleChange}
                     className="form-input px-3 py-2 border border-gray-300 rounded"
                     placeholder="Address"
                 />
-                <select className="form-select px-3 py-2 border border-gray-300 rounded">
+                <select
+                    name="role"
+                    value={updatedCustomer.role}
+                    onChange={handleChange}
+                    className="form-select px-3 py-2 border border-gray-300 rounded"
+                >
                     <option value="customer">Customer</option>
                     <option value="admin">Admin</option>
                 </select>
-                <button className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600">
+                <button type="submit" className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600">
                     Submit
                 </button>
             </form>
-
         </div>
     );
 };
