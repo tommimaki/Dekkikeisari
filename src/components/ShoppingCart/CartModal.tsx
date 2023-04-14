dfeb09ec1758c20817f9593eb162fe52029a4d0d
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-04-14 20:07:43 +0300

    made shopping cart responsive for mobile

--- src/components/ShoppingCart/CartModal.tsx
+++ src/components/ShoppingCart/CartModal.tsx
@@ -51,33 +51,61 @@ const CartModal = () => {
 
 
     return (
+        // <div className="cart-modal">
+        //     <div className="flex justify-end ">
+        //         <button onClick={handleCloseCart}>
+        //             <FaTimes />
+        //         </button>
+        //     </div>
+        //     <h2 className="text-lg font-bold mb-4">Cart</h2>
+
+        //     <div className="grid grid-cols-12 gap-4 mb-4 ">
+        //         <div className="col-span-2">
+        //             <p className="font-semibold text-gray-700">Product</p>
+        //         </div>
+        //         <div className="col-span-2">
+        //             <p className="font-semibold text-gray-700">Price</p>
+        //         </div>
+        //         <div className="col-span-2">
+        //             <p className="font-semibold text-gray-700">Size</p>
+        //         </div>
+        //         <div className="col-span-2">
+        //             <p className="font-semibold text-gray-700">Quantity</p>
+        //         </div>
+        //         <div className="col-span-2">
+        //             <p className="font-semibold text-gray-700">Total</p>
+        //         </div>
+        //         <div className="col-span-2">
+        //             <p className="font-semibold text-gray-700"></p>
+        //         </div>
         <div className="cart-modal">
-            <div className="flex justify-end ">
+            <div className="flex justify-end">
                 <button onClick={handleCloseCart}>
                     <FaTimes />
                 </button>
             </div>
             <h2 className="text-lg font-bold mb-4">Cart</h2>
 
-            <div className="grid grid-cols-12 gap-4 mb-4 ">
-                <div className="col-span-2">
+            <div className="flex flex-col md:grid md:grid-cols-12 gap-4 mb-4">
+                <div className="hidden md:block md:col-span-2">
                     <p className="font-semibold text-gray-700">Product</p>
                 </div>
-                <div className="col-span-2">
+                <div className="hidden md:block md:col-span-2">
                     <p className="font-semibold text-gray-700">Price</p>
                 </div>
-                <div className="col-span-2">
+                <div className="hidden md:block md:col-span-2">
                     <p className="font-semibold text-gray-700">Size</p>
                 </div>
-                <div className="col-span-2">
+                <div className="hidden md:block md:col-span-2">
                     <p className="font-semibold text-gray-700">Quantity</p>
                 </div>
-                <div className="col-span-2">
+                <div className="hidden md:block md:col-span-2">
                     <p className="font-semibold text-gray-700">Total</p>
                 </div>
-                <div className="col-span-2">
+                <div className="hidden md:block md:col-span-2">
                     <p className="font-semibold text-gray-700"></p>
                 </div>
+
 
 
 
@@ -106,25 +134,67 @@ const CartModal = () => {
 
                         const imageUrl = imageUrlsArray.length > 0 ? imageUrlsArray[0] : 'default_image_url';
 
+                        // return (
+                        //     <React.Fragment key={item.id}>
+                        //         <div className="col-span-2">
+                        //             <img src={imageUrl} alt={item.name} className="w-full rounded-lg mb-2" />
+                        //             <p className="font-semibold">{item.name}</p>
+                        //         </div>
+                        //         <div className="col-span-2">
+                        //             <p>${price.toFixed(2)}</p>
+                        //         </div>
+                        //         <div className="col-span-2">
+                        //             <p>{item.size}</p>
+                        //         </div>
+                        //         <div className="col-span-2">
+                        //             <p>{item.quantity}</p>
+                        //         </div>
+                        //         <div className="col-span-2">
+                        //             <p>${(price * (item.quantity ?? 0)).toFixed(2)}</p>
+                        //         </div>
+                        //         <div className="col-span-2">
+                        //             <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                        //                 onClick={() => handleRemoveFromCart(item)}
+                        //             >
+                        //                 Remove
+                        //             </button>
+                        //         </div>
+                        //     </React.Fragment>
+                        // );
                         return (
                             <React.Fragment key={item.id}>
-                                <div className="col-span-2">
+                                <div className="flex flex-col md:flex-none md:col-span-2">
+                                    <div className="flex md:hidden">
+                                        <p className="font-semibold text-gray-700 mr-2">Product:</p>
+                                    </div>
                                     <img src={imageUrl} alt={item.name} className="w-full rounded-lg mb-2" />
                                     <p className="font-semibold">{item.name}</p>
                                 </div>
-                                <div className="col-span-2">
+                                <div className="flex items-center md:items-start md:col-span-2">
+                                    <div className="flex md:hidden">
+                                        <p className="font-semibold text-gray-700 mr-2">Price:</p>
+                                    </div>
                                     <p>${price.toFixed(2)}</p>
                                 </div>
-                                <div className="col-span-2">
+                                <div className="flex items-center md:items-start md:col-span-2">
+                                    <div className="flex md:hidden">
+                                        <p className="font-semibold text-gray-700 mr-2">Size:</p>
+                                    </div>
                                     <p>{item.size}</p>
                                 </div>
-                                <div className="col-span-2">
+                                <div className="flex items-center md:items-start md:col-span-2">
+                                    <div className="flex md:hidden">
+                                        <p className="font-semibold text-gray-700 mr-2">Quantity:</p>
+                                    </div>
                                     <p>{item.quantity}</p>
                                 </div>
-                                <div className="col-span-2">
+                                <div className="flex items-center md:items-start md:col-span-2">
+                                    <div className="flex md:hidden">
+                                        <p className="font-semibold text-gray-700 mr-2">Total:</p>
+                                    </div>
                                     <p>${(price * (item.quantity ?? 0)).toFixed(2)}</p>
                                 </div>
-                                <div className="col-span-2">
+                                <div className="flex items-center md:items-start md:col-span-2">
                                     <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                                         onClick={() => handleRemoveFromCart(item)}
                                     >
