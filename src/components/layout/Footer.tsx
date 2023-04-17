0863b240ab964494cb25ae9629eff030cef0026c
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-04-17 13:10:58 +0300

    footer edited

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,34 +1,64 @@
 import React from 'react';
-import skatelogo from '../../assets/skatelogo.jpeg'
+
 
 const Footer = () => {
     return (
-        <footer className="bg-white  shadow dark:bg-gray-900 ">
+
+        <footer className="bg-white shadow dark:bg-gray-900">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://tommimaki.com" target="_blank" rel="noopener noreferrer" className="flex items-center mb-4 sm:mb-0">
-                        <img src={skatelogo} className="h-8 rounded mr-3" alt="skate Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Dekkikeisari</span>
-                    </a>
-                    <ul className="flex flex-wrap pr-4 items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <div className="flex items-center justify-center w w-full">
+                    <ul className="flex flex-wrap  md:flex-row pr-4 sm:flex-col items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 justify-center">
                         <li>
-                            <a href="https://tommimaki.com" target="_blank" rel="noopener noreferrer" className=" hover:underline pr-4">About</a>
+                            <a href="https://www.tommimaki.com" target="_blank" rel="noopener noreferrer" className="hover:underline  pr-4">Yhteystiedot</a>
                         </li>
                         <li>
-                            <a href="https://tommimaki.com" target="_blank" rel="noopener noreferrer" className="hover:underline pr-4">Privacy Policy</a>
+                            <a href="https://www.tommimaki.com" target="_blank" rel="noopener noreferrer" className="hover:underline pr-4">Toimitusehdot</a>
                         </li>
                         <li>
-                            <a href="https://tommimaki.com" target="_blank" rel="noopener noreferrer" className="hover:underline pr-4">Licensing</a>
+                            <a href="https://www.tommimaki.com" target="_blank" rel="noopener noreferrer" className="hover:underline pr-4">Asiakaspalautukset</a>
                         </li>
                         <li>
-                            <a href="https://tommimaki.com" target="_blank" rel="noopener noreferrer" className="hover:underline pr-4">Contact</a>
+                            <a href="https://www.tommimaki.com" target="_blank" rel="noopener noreferrer" className="hover:underline pr-4">Tietosuojaseloste</a>
+                        </li>
+                        <li>
+                            <a href="https://www.tommimaki.com" target="_blank" rel="noopener noreferrer" className="hover:underline pr-4">Dekkikeisari Forum</a>
                         </li>
                     </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://tommimaki.com" className="hover:underline">Tommi Mäki</a>. All Rights Reserved.</span>
+                <div className="text-sm text-gray-500 sm:text-center dark:text-gray-400 justify-center">
+                    <div className="grid grid-cols-1 md:grid-cols-3   gap-4">
+                        <div>
+                            <p>
+                                Katuosoite 123<br />
+                                Ma-Pe: 10-19<br />
+                                La: 10-18<br />
+                                Su: 12-17<br />
+                            </p>
+                        </div>
+                        <div>
+                            <p>
+                                <strong >Asiakastuki </strong> <br />
+                                050 2345 678<br />
+                                info@dekkikeisari.fi<br />
+                            </p>
+                        </div>
+                        <div>
+                            <p>
+                                Dekkikeisari Verkkokauppa<br />
+                                www.dekkikeisari.fi<br />
+
+                                verkkokauppa@dekkikeisari.fi<br />
+                            </p>
+                        </div>
+                        <div className="md:col-span-3 ">
+                            <p>© 2023 Dekkikeisari. Kaikki oikeudet pidätetään.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </footer >
+        </footer>
+
 
 
     );
